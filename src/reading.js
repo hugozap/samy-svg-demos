47e70213fbc4c255d89cd90b08b04a128a94bfdc
Hugo Zapata <hugozap@gmail.com>
2017-01-28 22:53:45 -0500

    cat anim

--- src/reading.js
+++ src/reading.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Samy, Proxy} from 'react-samy-svg'
 import reading from './reading_man.svg'
-import {Motion, spring}  from 'react-motion';
+import {Motion, spring} from 'react-motion'
 
 // NOTE: the svg cannot be distributed
 export default class Reading extends React.Component {
@@ -12,26 +12,48 @@ export default class Reading extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-        leftEyeOpened: true,
-        rightEyeOpened: true,
-        legAngle:0,
-        handleAngle: 0
+      leftEyeOpened: true,
+      rightEyeOpened: true,
+      legAngle: 0,
+      handleAngle: 0,
+      legAngle: 0,
+      catHeadAngle: 0
     }
   }
 
   componentDidMount (props) {
     this.animateEyes()
     this.animateHandle()
+    this.animateLeg()
+    this.animateCatHead()
   }
 
-    animateHandle() {
-        const changeAngle = (state)=> {
-            return { handleAngle : state.handleAngle === 1 ? -1 : 1 } 
-        }
-        setInterval( () => { 
-            this.setState(changeAngle)
-        }, 800)
+  animateLeg () {
+    const changeAngle = (state) => {
+      return { legAngle: state.legAngle === 5 ? -5 : 5 }
     }
+    setInterval(() => {
+      this.setState(changeAngle)
+    }, 2500)
+  }
+
+  animateCatHead () {
+    const changeAngle = (state) => {
+      return { catHeadAngle: state.catHeadAngle === 5 ? -5 : 5 }
+    }
+    setInterval(() => {
+      this.setState(changeAngle)
+    }, 3500)
+  }
+
+  animateHandle () {
+    const changeAngle = (state) => {
+      return { handleAngle: state.handleAngle === 1 ? -1 : 1 }
+    }
+    setInterval(() => {
+      this.setState(changeAngle)
+    }, 800)
+  }
   animateEyes () {
     const animateLeftEye = (state, props) => {
       return {leftEyeOpened: !state.leftEyeOpened}
@@ -41,15 +63,19 @@ export default class Reading extends React.Component {
     }
 
     setInterval(() => {
-     //Open
+     // Open
       this.setState(animateLeftEye)
       this.setState(animateRightEye)
       setTimeout(() => {
         // Close
         this.setState(animateLeftEye)
         this.setState(animateRightEye)
-      },300)
+      }, 300)
     }, 3000)
+  }
+
+  clickHandle (ev) {
+      console.log('hey')
   }
 
   render () {
@@ -57,26 +83,55 @@ export default class Reading extends React.Component {
     // TODO: create utils for transforms/scale/rotate
     const leye = `scale(1,${s.leftEyeOpened ? 1 : 0.2})`
     const reye = `scale(1,${s.rightEyeOpened ? 1 : 0.2})`
-    const handle = (svg)=>{
-        
-        return (
-        <Motion defaultStyle={{a:0}} style={{a:spring(s.handleAngle)}}>
-            {
-                (val)=>{
-                    const transform = `rotate(${val.a}, 10, 0)`
-                    return <Proxy svg={svg} select="#handle" transform={transform} />
+    const catHead = (svg) => {
+      return (
+        <Motion defaultStyle={{a: 0}} style={{a: spring(s.catHeadAngle)}}>
+          {
+                (val) => {
+                  const transform = `rotate(${val.a}, 15, 15)`
+                  return <Proxy svg={svg} select='#cathead' transform={transform} />
                 }
             }
         </Motion>
-        
-    )}
+
+      )
+    }
+
+    const handle = (svg) => {
+      return (
+        <Motion defaultStyle={{a: 0}} style={{a: spring(s.handleAngle)}}>
+          {
+                (val) => {
+                  const transform = `rotate(${val.a}, 10, 0)`
+                  return <Proxy svg={svg} onclick={this.clickHandle} select='#handle' transform={transform} />
+                }
+            }
+        </Motion>
+
+      )
+    }
+    const leg = (svg) => {
+      return (
+        <Motion defaultStyle={{a: 0}} style={{a: spring(s.legAngle)}}>
+          {
+                (val) => {
+                  const transform = `rotate(${val.a}, 10, 0)`
+                  return <Proxy svg={svg} select='#leg' transform={transform} />
+                }
+            }
+        </Motion>
+
+      )
+    }
 
     return (
       <Samy path={reading} style={{width: '100%', height: '100vh'}}>
         { (svg) => [
           <Proxy svg={svg} select='#lefteye' transform={leye} />,
           <Proxy svg={svg} select='#righteye' transform={reye} />,
-          handle(svg)
+          handle(svg),
+          leg(svg),
+          catHead(svg)
         ]
 
         }
